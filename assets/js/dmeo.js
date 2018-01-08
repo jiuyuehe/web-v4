c902b593088ebd312ee68b03350e6004a336786a
liming1996 <email@1425590821@qq.com>
2018-01-08 10:36:13 +0800

    xiugai

---
+++ assets/js/dmeo.js
@@ -0,0 +1,592 @@
+//price
+var cont1 = [
+    //3.6
+    {
+        type: 1,
+        img: "../assets/images/pr-fx.png",
+        name: "优化我的分享模块"
+    },
+    {
+        type: 2,
+        img: "../assets/images/pr-mk.png",
+        name: "增加独立的系统模块"
+    },
+    {
+        type: 3,
+        img: "../assets/images/pr-fd.png",
+        name: "防盗链功能升级"
+    },
+    {
+        type: 4,
+        img: "../assets/images/pr-sy.png",
+        name: "增加水印功能"
+    },
+    {
+        type: 5,
+        img: "../assets/images/pr-bmd.png",
+        name: "增加白名单功能"
+    },
+    {
+        type: 6,
+        img: "../assets/images/pr-sy.png",
+        name: "开放共享空间多级授权功能"
+    },
+    {
+        type: 7,
+        img: "../assets/images/pr-bd.png",
+        name: "客户端新增开启启动配置，修改注册表"
+    },
+]
+var cont2 = [
+    //3.5
+    {
+        type: 1,
+        img: "../assets/images/p-wx.png",
+        name: "UI更新"
+    },
+    {
+        type: 2,
+        img: "../assets/images/p-dhl.png",
+        name: "增加导航栏跳转功能"
+    },
+    {
+        type: 3,
+        img: "../assets/images/p-y.png",
+        name: "增加右键排序与查看模式"
+    },
+    {
+        type: 4,
+        img: "../assets/images/p-jm.png",
+        name: "使用标准的AES方式加密"
+    },
+    {
+        type: 5,
+        img: "../assets/images/p-bj.png",
+        name: "增加关联引用文件编辑功能"
+    }
+]
+var cont3 = [
+    //3.4
+    {
+        type: 1,
+        img: "../assets/images/bug-2.0.png",
+        name: "bugfix"
+    },
+    {
+        type: 2,
+        img: "../assets/images/qx-3.4.png",
+        name: "升级权限功能"
+    },
+    {
+        type: 3,
+        img: "../assets/images/bm-3.4.png",
+        name: "增加以个人属于多个部门"
+    },
+    {
+        type: 4,
+        img: "../assets/images/dd-3.4.png",
+        name: "发布钉钉同步组件，加云盘与企业钉钉账户关联"
+    },
+]
+var cont4 = [
+    //3.3
+    {
+        type: 1,
+        img: "../assets/images/bug-2.0.png",
+        name: "bugfix"
+    },
+    {
+        type: 2,
+        img: "../assets/images/llq-3.3.png",
+        name: "PC端使用嵌入浏览器方式"
+    },
+    {
+        type: 3,
+        img: "../assets/images/wx-3.3.png",
+        name: "优化移动端外链展示，微信增加提示"
+    },
+    {
+        type: 4,
+        img: "../assets/images/lj-3.3.png",
+        name: "PC端使用嵌入浏览器方式"
+    },
+]
+var cont5 = [
+    //3.2
+    {
+        type: 1,
+        img: "../assets/images/bug-2.0.png",
+        name: "bugfix"
+    },
+    {
+        type: 2,
+        img: "../assets/images/ip-3.2.png",
+        name: "增加ip地址自动适配"
+    },
+    {
+        type: 3,
+        img: "../assets/images/ys-3.2.png",
+        name: "增加基于dnspod的DDNS映射"
+    },
+    {
+        type: 4,
+        img: "../assets/images/bj-3.2.png",
+        name: "增加网页调用客户端对文件进行在线编辑功能"
+    },
+]
+var cont6 = [
+    //3.0
+    {
+        type: 1,
+        img: "../assets/images/bug-2.0.png",
+        name: "bugfix"
+    },
+    {
+        type: 2,
+        img: "../assets/images/fx-3.0.png",
+        name: "增加企业内部成员分享"
+    },
+    {
+        type: 3,
+        img: "../assets/images/tz-3.0.png",
+        name: "增加基于网页的拖拽移动功能"
+    },
+    {
+        type: 4,
+        img: "../assets/images/ss-3.0.png",
+        name: "增加全文搜索（独立组件）"
+    },
+    {
+        type: 5,
+        img: "../assets/images/gl-3.0.png",
+        name: "大幅度优化文件管理性能"
+    },
+]
+var cont7 = [
+    //2.4
+    {
+        type: 1,
+        img: "../assets/images/bug-2.0.png",
+        name: "bugfix"
+    },
+    {
+        type: 2,
+        img: "../assets/images/bu-2.4.png",
+        name: "文件同步功能"
+    },
+    {
+        type: 3,
+        img: "../assets/images/ss-2.4.png",
+        name: "搜索功能重构"
+    },
+    {
+        type: 4,
+        img: "../assets/images/bj-2.4.png",
+        name: "多人协作编辑，文件锁定"
+    },
+    {
+        type: 5,
+        img: "../assets/images/hs-2.4.png",
+        name: "增加系统回收功能，增加多同步盘符"
+    },
+]
+var cont8 = [
+    //2.0
+    {
+        type: 1,
+        img: "../assets/images/bug-2.0.png",
+        name: "bugfix"
+    },
+    {
+        type: 2,
+        img: "../assets/images/ui-2.0.png",
+        name: "使用全新UI主题"
+    },
+    {
+        type: 3,
+        img: "../assets/images/mac-2.0.png",
+        name: "移动端、mac端上线"
+    },
+    {
+        type: 4,
+        img: "../assets/images/b-2.0.png",
+        name: "发布在线安装版本，在线安装版不到100M"
+    },
+]
+//nas
+var nas = [
+    {
+        type: 1,
+        span: "CPU",
+        p: "Intel四核2.3GHz"
+    },
+    {
+        type: 2,
+        span: "内存容量",
+        p: "2GB DDR3 RAM"
+    },
+    {
+        type: 3,
+        span: "高性能",
+        p: "读取226MB/S <br>写入184MB/S"
+    },
+    {
+        type: 4,
+        span: "扩展性",
+        p: "双通道4K H.264/H.265S视频转码"
+    }
+]
+//s-nas
+var prog = [
+    {
+        type: 1,
+        span: "CPU",
+        p: "Intel四核2.3GHz"
+    },
+    {
+        type: 2,
+        span: "内存容量",
+        p: "2GB DDR3 RAM"
+    },
+    {
+        type: 3,
+        span: "高性能",
+        p: "读取226MB/S <br>写入184MB/S"
+    },
+    {
+        type: 4,
+        span: "扩展性",
+        p: "双通道4K H.264/H.265S视频转码"
+    }
+]
+//nav
+var prod2 = [
+    {
+        type: 1,
+        name: "小机器1",
+        href: "nas.html",
+        src: "../assets/images/小机器1.png",
+        price: "￥2000"
+    },
+    {
+        type: 2,
+        name: "小机器2",
+        href: "s-nas.html",
+        src: "../assets/images/小机器2.png",
+        price: "￥2000"
+    },
+    {
+        type: 3,
+        name: "小机器3",
+        href: "product.html",
+        src: "../assets/images/小机器3.png",
+        price: "￥2000"
+    },
+    {
+        type: 4,
+        name: "云主机",
+        href: "product.html",
+        src: "../assets/images/云主机.png",
+        price: "￥2000"
+    }
+]
+var mach = [
+    {
+        type: 1,
+        span: "CPU",
+        p: "Intel四核2.3GHz",
+        src: "../assets/images/2.png"
+    },
+    {
+        type: 2,
+        span: "内存容量",
+        p: "2GB DDR3 RAM",
+        src: "../assets/images/3.png"
+    },
+    {
+        type: 3,
+        span: "高性能",
+        p: "读取450MB/S <br>写入400MB/S",
+        src: "../assets/images/4.png"
+    },
+    {
+        type: 4,
+        span: "扩展性",
+        p: "可扩充16个硬盘",
+        src: "../assets/images/5.png"
+    },
+]
+//about
+var exh = [
+    {
+        type: 1,
+        href: "ding.html",
+        src: "../assets/images/a-dd.png",
+        name: "钉钉集成",
+        summary: "实现钉钉用户免登云盘，并能一键导入组织架构，避免重复工作。",
+        p: "点击了解详情>"
+    },
+    {
+        type: 2,
+        href: "xz.html",
+        src: "../assets/images/a-xz.png",
+        name: "协作办公",
+        summary: "实现钉钉用户免登云盘，并能一键导入组织架构，避免重复工作。",
+        p: "点击了解详情>"
+    },
+    {
+        type: 3,
+        href: "knowLedge.html",
+        src: "../assets/images/a-zsk.png",
+        name: "知识库",
+        summary: "实现钉钉用户免登云盘，并能一键导入组织架构，避免重复工作。",
+        p: "点击了解详情>"
+    },
+]
+//
+function getcon1(num) {
+    for (var i = 0; i < cont1.length; i++) {
+
+        var html =
+            '<div class="col-md-4 col-sm-4 m-product">' +
+            '<div class="m-product-img">' +
+            '<img class="center-block" src="' + cont1[i].img + '" alt="">' +
+            '</div>' +
+            '<div class="m-product-con">' +
+            '<h5>' + cont1[i].name + '</h5>' +
+            '</div>' +
+            '</div>'
+
+        $("#cont1").append(html);
+
+    }
+}
+function getcon2(num) {
+    for (var i = 0; i < cont2.length; i++) {
+
+        var html =
+            '<div class="col-md-4 col-sm-4 m-product">' +
+            '<div class="m-product-img">' +
+            '<img class="center-block" src="' + cont2[i].img + '" alt="">' +
+            '</div>' +
+            '<div class="m-product-con">' +
+            '<h5>' + cont2[i].name + '</h5>' +
+            '</div>' +
+            '</div>'
+
+        $("#cont2").append(html);
+
+    }
+}
+function getcon3(num) {
+    for (var i = 0; i < cont3.length; i++) {
+
+        var html =
+            '<div class="col-md-4 col-sm-4 m-product">' +
+            '<div class="m-product-img">' +
+            '<img class="center-block" src="' + cont3[i].img + '" alt="">' +
+            '</div>' +
+            '<div class="m-product-con">' +
+            '<h5>' + cont3[i].name + '</h5>' +
+            '</div>' +
+            '</div>'
+
+        $("#cont3").append(html);
+
+    }
+}
+function getcon4(num) {
+    for (var i = 0; i < cont4.length; i++) {
+
+        var html =
+            '<div class="col-md-4 col-sm-4 m-product">' +
+            '<div class="m-product-img">' +
+            '<img class="center-block" src="' + cont4[i].img + '" alt="">' +
+            '</div>' +
+            '<div class="m-product-con">' +
+            '<h5>' + cont4[i].name + '</h5>' +
+            '</div>' +
+            '</div>'
+
+        $("#cont4").append(html);
+
+    }
+}
+function getcon5(num) {
+    for (var i = 0; i < cont5.length; i++) {
+
+        var html =
+            '<div class="col-md-4 col-sm-4 m-product">' +
+            '<div class="m-product-img">' +
+            '<img class="center-block" src="' + cont5[i].img + '" alt="">' +
+            '</div>' +
+            '<div class="m-product-con">' +
+            '<h5>' + cont5[i].name + '</h5>' +
+            '</div>' +
+            '</div>'
+
+        $("#cont5").append(html);
+
+    }
+}
+function getcon6(num) {
+    for (var i = 0; i < cont6.length; i++) {
+
+        var html =
+            '<div class="col-md-4 col-sm-4 m-product">' +
+            '<div class="m-product-img">' +
+            '<img class="center-block" src="' + cont6[i].img + '" alt="">' +
+            '</div>' +
+            '<div class="m-product-con">' +
+            '<h5>' + cont6[i].name + '</h5>' +
+            '</div>' +
+            '</div>'
+
+        $("#cont6").append(html);
+
+    }
+}
+function getcon7(num) {
+    for (var i = 0; i < cont7.length; i++) {
+
+        var html =
+            '<div class="col-md-4 col-sm-4 m-product">' +
+            '<div class="m-product-img">' +
+            '<img class="center-block" src="' + cont7[i].img + '" alt="">' +
+            '</div>' +
+            '<div class="m-product-con">' +
+            '<h5>' + cont7[i].name + '</h5>' +
+            '</div>' +
+            '</div>'
+
+        $("#cont7").append(html);
+
+    }
+}
+function getcon8(num) {
+    for (var i = 0; i < cont8.length; i++) {
+
+        var html =
+            '<div class="col-md-4 col-sm-4 m-product">' +
+            '<div class="m-product-img">' +
+            '<img class="center-block" src="' + cont8[i].img + '" alt="">' +
+            '</div>' +
+            '<div class="m-product-con">' +
+            '<h5>' + cont8[i].name + '</h5>' +
+            '</div>' +
+            '</div>'
+
+        $("#cont8").append(html);
+
+    }
+}
+//s-nas
+function getprog(num) {
+    for (var i = 0; i < prog.length; i++) {
+        var html =
+            '<div class="col-md-3 col-sm-3 m-prog-content">' +
+            '<div class="m-prog-cont">' +
+            '<span>' + prog[i].span + '</span>' +
+            '<p>' + prog[i].p + '</p>' +
+            '</div>' +
+            '</div>'
+
+        $("#prog").append(html);
+    }
+}
+//nas
+function getnas(num) {
+    for (var i = 0; i < nas.length; i++) {
+        var html =
+            '<div class="col-md-3 col-sm-3 m-prog-content">' +
+            '<div class="m-prog-cont">' +
+            '<span>' + nas[i].span + '</span>' +
+            '<p>' + nas[i].p + '</p>' +
+            '</div>' +
+            '</div>'
+
+        $("#n-nas").append(html);
+    }
+}
+
+//about
+function getexh(num) {
+    for (var i = 0; i < exh.length; i++) {
+        var html =
+            '<div class="col-md-4">' +
+            '<div class="e-content">' +
+            '<a href="' + exh[i].href + '"><img src="' + exh[i].src + '" alt=""></a>' +
+            '<h3><b>' + exh[i].name + '</b></h3>' +
+            '<p>' + exh[i].summary + '</p>' +
+            '</div>' +
+            '<div class="e-content2">' +
+            '<h3><b>' + exh[i].name + '</b></h3>' +
+            '<p>' + exh[i].summary + '</p>' +
+            '<a href="' + exh[i].href + '">' +
+            '<p>' + exh[i].p + '></p>' +
+            '</a>' +
+            '<a href="' + exh[i].href + '"><img src="' + exh[i].src + '" alt=""></a>' +
+            '</div>' +
+            '</div>'
+
+        $("#exh").append(html);
+    }
+}
+//nav
+function getprod2(num) {
+    for (var i = 0; i < prod2.length; i++) {
+        var html =
+            '<div class="col-md-3 col-sm-3 col-xs-3 m-product">' +
+            '<div class="n-product-img">' +
+            '<a href="' + prod2[i].href + '"> <img class="center-block" src="' + prod2[i].src + '" alt=""></a>' +
+            '</div>' +
+            '<div class="m-product-con">' +
+            '<h5>' + prod2[i].name + '</h5>' +
+            '<span>' + prod2[i].price + '</span>' +
+            '</div>' +
+            '</div>'
+
+        $("#prod2").append(html);
+    }
+}
+//mach
+function getmach(num) {
+    for (var i = 0; i < mach.length; i++) {
+        var html =
+            '<div class="col-md-3 col-sm-3 m-prog-content">' +
+            '<div class="m-prog-cont">' +
+            '<span>' + mach[i].span + '</span>' +
+            '<p>' + mach[i].p + '</p>' +
+            '</div>' +
+            '<div class="m-img">' +
+            '<img src="' + mach[i].src + '" alt="">' +
+            '</div>' +
+            '</div>'
+
+        $("#mach").append(html);
+    }
+}
+$(document).ready(function () {
+    console.log("start ---------");
+
+    //getExams(0);
+    //getUs(0);
+    //getPro(0);
+    // getPro2(0);
+    //getPri(0);
+    //getsNas(0);
+    //getNas(0);
+    getcon1(0);
+    getcon2(0);
+    getcon3(0);
+    getcon4(0);
+    getcon5(0);
+    getcon6(0);
+    getcon7(0);
+    getcon8(0);
+    getprog(0);
+    getnas(0);
+    getexh(0);
+    getprod2(0);
+    getmach(0);
+});
