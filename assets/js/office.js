b66f431e00db92d0a85b0f1e722c2208e8a3d613
jiuyuehe <lucene@foxmail.com>
2018-01-22 17:37:48 +0800

    release - fix1 -v1.0

---
+++ assets/js/office.js
@@ -0,0 +1,44 @@
+
+
+console.log("window.location",window.location);
+
+if(window.location.pathname.indexOf('xz-office')>0){
+
+
+
+
+    // var  ifurl = undefined;
+
+
+    if(window.location.search && window.location.search.indexOf("word") > 0){
+
+       $('iframe').attr('src',"http://www.szchangxun.cn:8090/cloud-office/api/local/test.docx/open?app=WRITE_VIEWER&root=/home/hancom&fid=1111&user_id=lockfree"+(Math.floor(Math.random()*52 + 1)%3))
+
+    }else if(window.location.search && window.location.search.indexOf("excel") > 0){
+
+        $('iframe').attr('src'," http://www.szchangxun.cn:8090/cloud-office/api/local/test.xlsx/open?app=CALC_EDITOR&root=/home/hancom&fid=222&user_id=lockfree"+(Math.floor(Math.random()*52 + 1)%3))
+
+    }else{
+
+        $('iframe').attr('src',"http://www.szchangxun.cn:8090/cloud-office/api/local/test.pptx/open?app=SHOW_EDITOR&root=/home/hancom&fid=333&user_id=lockfree"+(Math.floor(Math.random()*52 + 1)%3))
+
+    }
+
+
+    // var iframe = document.createElement("iframe");
+    // iframe.src = "http://www.baidu.com";
+    //
+    // if (iframe.attachEvent){
+    //     iframe.attachEvent("onload", function(){ // IE
+    //         alert("Local iframe is now loaded.");
+    //     });
+    // } else {
+    //     iframe.onload = function(){ // 非IE
+    //         alert("Local iframe is now loaded.");
+    //     };
+    // }
+    //
+    // document.body.appendChild(iframe);
+
+
+}
